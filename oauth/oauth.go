f1f6ee986b62d98cf200c39f14d8dcd95c213c65
Tulio Tanikawa <tulio_t@hotmail.com>
2023-01-03 07:58:53 -0300

    Refactor error handling

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -2,6 +2,7 @@ package oauth
 
 import (
 	"encoding/json"
+	"errors"
 
 	"fmt"
 	"net/http"
@@ -10,7 +11,7 @@ import (
 	"time"
 
 	"github.com/mercadolibre/golang-restclient/rest"
-	"github.com/ttanik/bookstore_oauth-go/oauth/errors"
+	"github.com/ttanik/bookstore_utils-go/rest_errors"
 )
 
 const (
@@ -63,7 +64,7 @@ func GetClientId(request *http.Request) int64 {
 	return clientId
 }
 
-func AuthenticateRequest(request *http.Request) *errors.RestErr {
+func AuthenticateRequest(request *http.Request) rest_errors.RestErr {
 	if request == nil {
 		return nil
 	}
@@ -77,7 +78,7 @@ func AuthenticateRequest(request *http.Request) *errors.RestErr {
 
 	at, err := getAccessToken(accessTokenId)
 	if err != nil {
-		if err.Status == http.StatusNotFound {
+		if err.GetStatus() == http.StatusNotFound {
 			return nil
 		}
 		return err
@@ -95,23 +96,23 @@ func cleanRequest(request *http.Request) {
 	request.Header.Del(headerXCallerId)
 }
 
-func getAccessToken(accessTokenId string) (*accessToken, *errors.RestErr) {
+func getAccessToken(accessTokenId string) (*accessToken, rest_errors.RestErr) {
 	response := oauthRestClient.Get(fmt.Sprintf("/oauth/access_token/%s", accessTokenId))
 	if response == nil || response.Response == nil {
-		return nil, errors.NewInternalServerError("invalid restclient response when trying to get access token")
+		return nil, rest_errors.NewInternalServerError("invalid restclient response when trying to get access token", errors.New("request error"))
 	}
 
 	if response.StatusCode > 299 {
 		if response.StatusCode == http.StatusNotFound {
-			return nil, errors.NewNotFoundError("not found")
+			return nil, rest_errors.NewNotFoundError("not found")
 		}
-		return nil, errors.NewInternalServerError("invalid error interface when trying to get access token")
+		return nil, rest_errors.NewInternalServerError("invalid error interface when trying to get access token", errors.New("not found error"))
 
 	}
 
 	var at accessToken
 	if err := json.Unmarshal(response.Bytes(), &at); err != nil {
-		return nil, errors.NewInternalServerError("error when trying to unmarshal access token response")
+		return nil, rest_errors.NewInternalServerError("error when trying to unmarshal access token response", errors.New("json format error"))
 	}
 	return &at, nil
 }
